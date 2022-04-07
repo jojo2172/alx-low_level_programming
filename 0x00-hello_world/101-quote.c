87e7c5c2b4729ffa5a178141b224d75d9073a1ac
jojo2172 <yohannes2172@gmail.com>
2022-04-07 14:36:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
 /**
@@ -14,8 +13,8 @@ int main(void)
 
 {
 
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
-	return (1);
+return (1);
 
 }
