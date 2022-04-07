2b6dfce1bc092f2d4e7f188ce4e93e45503b3844
jojo2172 <yohannes2172@gmail.com>
2022-04-07 13:40:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,28 +1,4 @@
-#!/bin/bash
-<<<<<<< HEAD
-#!/bin/bash                                                                                                                             
-
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                                     
- *
- *  *  * main - main block                                                                                                                 
- *
- *   *   * Return: 0                                                                                                                        
- *
- *    *    */                                                                                                                                
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	        puts("\"Programming is like building a multilingual puzzle");                                                                   
-
-		        return (0);                                                                                                                     
-
-}
-=======
-#include <stdio.h>
+include <stdio.h>
 /**
  *  * main - main block
  *   * Return: 0
@@ -32,5 +8,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
->>>>>>> 23fc56a1cc76526585db293a7faba3bea7d16b77
